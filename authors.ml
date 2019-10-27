9499070cc944947bacf123669f1dc09701643380
atw54 <atw54@cornell.edu>
2019-10-27 15:46:41 -0400

    Initial author files

---
+++ authors.ml
@@ -0,0 +1,3 @@
+(** Hours worked for Drew Williams (atw54) , Eric Dusenberry (efd44) , 
+    Haoxuan Chen (hc677) , and Daniel Zhan (dz268), respectively.*)
+let hours_worked = [0;0;0;0]
---
+++ authors.mli
@@ -0,0 +1,7 @@
+(** 
+    Metadata about this submission.
+    @author Drew Williams (atw54) , Eric Dusenberry (efd44) , 
+    Haoxuan Chen (hc677) , Daniel Zhan (dz268) 
+*)
+
+val hours_worked: int list
