d84b6468529578f92ca6f57b32340fd424a479a2
atw54 <atw54@cornell.edu>
2019-10-30 21:29:17 -0400

    MS1 Meeting 2

--- authors.ml
+++ authors.ml
@@ -1,3 +1,3 @@
 (** Hours worked for Drew Williams (atw54) , Eric Dusenberry (efd44) , 
     Haoxuan Chen (hc677) , and Daniel Zhan (dz268), respectively.*)
-let hours_worked = [3;0;3;3]
+let hours_worked = [6;3;6;6]
