00e531fd62c5eff43f2dff1e1ba1c9c8605f351e
Eric Dusenberry <efd44@cornell.edu>
2019-12-10 23:24:24 -0500

    Changed hours for MS3

--- authors.ml
+++ authors.ml
@@ -1,3 +1,3 @@
 (** Hours worked for Drew Williams (atw54) , Eric Dusenberry (efd44) , 
     Haoxuan Chen (hc677) , and Daniel Zhan (dz268), respectively.*)
-let hours_worked = [20;11;18;30]
+let hours_worked = [20;15;18;30]
